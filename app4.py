b16188da34b4b70531016d278c2cf65797fd1958
Arsalan-Fard <63359673+Arsalan-Fard@users.noreply.github.com>
2025-04-24 19:35:50 +0330

    Update app4.py

--- app4.py
+++ app4.py
@@ -1,11 +1,12 @@
-from flask import Flask, redirect
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return redirect("http://127.0.0.1:5004/secret")
+    # Try to directly return content similar to what we expect from /secret
+    return 'This is what we would get from the /secret endpoint', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
